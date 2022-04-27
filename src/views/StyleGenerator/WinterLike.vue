c26ed62629cc08e61de112e843bf3314fa04ac9e
haoc <haoc@rihuisoft.com>
2022-04-27 16:47:13 +0800

    rabbit init

--- src/views/StyleGenerator/WinterLike.vue
+++ src/views/StyleGenerator/WinterLike.vue
@@ -457,9 +457,9 @@ yt-live-chat-text-message-renderer #message {
   display: block !important;
   border: 1px solid transparent;
   overflow: visible !important;
-  border-width: 90px 86px 0 52px !important;
-  border-image-slice: 270 258 0 157 fill !important;
-  padding: 0 0 66px 0;
+  border-width: 0 86px 0 52px !important;
+  border-image-slice: 0 258 0 157 fill !important;
+  padding: 90px 0 66px 0;
   border-image-source: url('/static/img/common/rabbitBody.png') !important;
 }`
     },
